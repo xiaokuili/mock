be538c26b08cee38045dc82762142dc78d7dae3f
yinlin1991 <lenglengzailuguan@gamil.com>
2024-05-13 19:58:24 +0800

    chore: Refactor continueConversation function to use message history instead of input string

--- my-ai-app/app/actions.ts
+++ my-ai-app/app/actions.ts
@@ -1,85 +1,26 @@
 "use server";
 
-import { createAI, getMutableAIState, streamUI } from "ai/rsc";
-import { createOpenAI, openai } from "@ai-sdk/openai";
-import { ReactNode } from "react";
-import { z } from "zod";
-import { nanoid } from "nanoid";
-import { Stock } from "@ai-studio/components/stock";
+import { createOpenAI } from "@ai-sdk/openai";
+import { CoreMessage, generateText, streamText } from "ai";
+import { string } from "zod";
+import { createStreamableValue } from "ai/rsc";
 
-export interface ServerMessage {
+const openai = createOpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+  baseURL: process.env.OPENAI_BASE_URL,
+});
+export interface Message {
   role: "user" | "assistant";
   content: string;
 }
-
-export interface ClientMessage {
-  id: string;
-  role: "user" | "assistant";
-  display: ReactNode;
-}
-
-export async function continueConversation(
-  input: string
-): Promise<ClientMessage> {
+export async function continueConversation(history: Message[]) {
   "use server";
-
-  const history = getMutableAIState();
-  const openai = createOpenAI({
-    baseURL: process.env.OPENAI_BASE_URL,
-    apiKey: process.env.OPENAI_API_KEY,
-  });
-  const div = (content: string) => `<div>${content}</div>`;
-  const result = await streamUI({
+  const { textStream } = await streamText({
     model: openai("gpt-3.5-turbo"),
-    messages: [...history.get(), { role: "user", content: input }],
-    text: ({ content, done }) => {
-      if (done) {
-        history.done((messages: ServerMessage[]) => [
-          ...messages,
-          { role: "assistant", content },
-        ]);
-      }
-
-      return div(content);
-    },
-    tools: {
-      showStockInformation: {
-        description:
-          "Get stock information for symbol for the last numOfMonths months",
-        parameters: z.object({
-          symbol: z
-            .string()
-            .describe("The stock symbol to get information for"),
-          numOfMonths: z
-            .number()
-            .describe("The number of months to get historical information for"),
-        }),
-        generate: async ({ symbol, numOfMonths }) => {
-          history.done((messages: ServerMessage[]) => [
-            ...messages,
-            {
-              role: "assistant",
-              content: `Showing stock information for ${symbol}`,
-            },
-          ]);
-
-          return Stock({ symbol: symbol, numOfMonths: numOfMonths });
-        },
-      },
-    },
+    system: "你是一个推荐电影的助手,每次回答在100字以内",
+    messages: history,
   });
+  const stream = createStreamableValue(textStream);
 
-  return {
-    id: nanoid(),
-    role: "assistant",
-    display: result.value,
-  };
+  return stream.value;
 }
-
-export const AI = createAI<ServerMessage[], ClientMessage[]>({
-  actions: {
-    continueConversation,
-  },
-  initialAIState: [],
-  initialUIState: [],
-});
