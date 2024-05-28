9e8b6df854dc9eb595e695bdfc0c9fdefc919899
yinlin1991 <lenglengzailuguan@gamil.com>
2024-05-28 11:48:23 +0800

    delete

--- my-ai-app/app/ui.tsx
+++
@@ -1,9 +0,0 @@
-const UpdateUI = () => {
-  return <div>Update...</div>;
-};
-
-const DoneUI = () => {
-  return <div>Done!</div>;
-};
-
-export { UpdateUI, DoneUI };
--- my-ai-app/app/weather.tsx
+++
@@ -1,3 +0,0 @@
-export default function Weather() {
-  return <div className="bg-blue-500 text-white w-16 h-8">Weather...</div>;
-}
